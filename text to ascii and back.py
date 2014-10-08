46055952bfadd79f96017498f4274896c75ab967
georgewest <georgewest@georges-mbp>
2014-10-08 19:39:14 +0100

    Ascii

---
+++ text to ascii and back.py
@@ -0,0 +1,12 @@
+#1
+num = int(input("Please input an ascii number: "))
+character=chr(num)
+print("The character is a {0}".format(character))
+
+
+#2
+char= input("Please enter a character: ")
+ascii1 =ord(char)
+print("The ascii number is {0}".format(ascii1))
+  
+
